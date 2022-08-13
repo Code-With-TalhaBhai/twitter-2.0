584a09a7cc22eeb72859d8f73d5ee8101f32f2e7
Muhammad Talha <talhakhalid411@gmail.com>
2022-08-13 16:42:27 +0500

    public base url

--- labels/fetchTweets.ts
+++ labels/fetchTweets.ts
@@ -1,7 +1,7 @@
 import { Tweet } from "../typings";
 
 export const fetchTweets = async()=>{
-    const data = await fetch(`${process.env.VERCEL_URL}/api/getTweets`);
+    const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`);
     console.log(data)
     const res = await data.json()
     const tweets:Tweet[] = res.AllTweets;
